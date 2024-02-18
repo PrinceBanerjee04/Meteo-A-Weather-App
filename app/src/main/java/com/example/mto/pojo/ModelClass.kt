f306982ee050f8d86100b29b092396407b7e9c13
Prince <banerjee.prince69@gmail.com>
2024-02-18 07:26:46 +0530

    POJO & UTILS

--- app/src/main/java/com/example/mto/pojo/ModelClass.kt
+++ app/src/main/java/com/example/mto/pojo/ModelClass.kt
@@ -3,5 +3,10 @@ package com.example.mto.pojo
 import com.google.gson.annotations.SerializedName
 
 data class ModelClass(
-    @SerializedName("weather") val weather:List<Weather>
+    @SerializedName("weather") val weather:List<Weather>,
+    @SerializedName("main") val main:Main,
+    @SerializedName("wind") val wind:Wind,
+    @SerializedName("sys") val sys:Sys,
+    @SerializedName("id") val id:Int,
+    @SerializedName("name") val name:String
 )
