def4d615d3eacfc26ee62f8545296c5502a04153
Prince <banerjee.prince69@gmail.com>
2024-02-18 06:31:37 +0530

    WIP

--- app/src/main/java/com/example/mto/MainActivity.kt
+++ app/src/main/java/com/example/mto/MainActivity.kt
@@ -26,5 +26,72 @@ class MainActivity : AppCompatActivity() {
         getCurrentLocation()
     }
 
+    private fun getCurrentLocation(){
+        if(checkPermissions())
+        {
+            if(isLocationEnabled())
+            {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermission()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task->
+
+                    val location:Location?=task.result
+                    if(location==null)
+                    {
+                        Toast.makeText(applicationContext,"Null",Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                    {
+                        Toast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            }
+            else
+            {
+                Toast.makeText(this,"Turn on Location",Toast.LENGTH_SHORT).show()
+                val intent=Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        else
+        {
+            requestPermission()
+        }
+    }
+
+    companion object{
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
+    }
+
+    private fun isLocationEnabled():Boolean{
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkPermissions():Boolean
+    {
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION)
+    }
+
 
 }
