5d968f6182dd119729876598199bfdf822c4f928
Prince <banerjee.prince69@gmail.com>
2024-02-18 06:30:58 +0530

    Commit

--- app/src/main/java/com/example/mto/MainActivity.kt
+++ app/src/main/java/com/example/mto/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.mto
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.media.audiofx.Equalizer.Settings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.concurrent.atomic.LongAccumulator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this)
+
+        getCurrentLocation()
     }
+
+
 }
